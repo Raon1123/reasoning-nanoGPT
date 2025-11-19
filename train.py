226d62cb43023d193cbcefba23adb118d21818f3
Yunpyo An <anyunpyo@unist.ac.kr>
2025-11-19 13:39:21 +0900

    get newer version of GPT form nanoChat

--- train.py
+++ train.py
@@ -27,9 +27,10 @@ import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 
-from model import GPTConfig, GPT
+from models.model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
+# TODO: fix the config for reasoning task
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
